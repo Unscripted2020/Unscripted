83b36473c508583d49ee39d4417b3bb9c5740854
Santiago Rendon <sr4670@nyu.edu>
2020-06-04 14:44:09 -0400

    seperating db initation and server

--- server/app.js
+++ server/app.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 7000;
-const mongoose = require('mongoose');
-const url = "mongodb://localhost:27017/unscripted"
+const port = 3000;
+require("./db.js");
 
-mongoose.connect(url,{
-	useNewUrlParser: "true",
-	useUnifiedTopology: "true",
-})
-.then(() => console.log(`connected to database: ${url}`))
-.catch(err =>{
-	console.log(err)
-});
+
 
 app.get('/', (req,res) => {
 	res.send("connected to app")
