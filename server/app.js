e68f827e0d8806569772d1d97397992252a90ea1
santiagorendon <sr4670@nyu.edu>
2020-06-07 18:51:30 -0400

    added login functionality

--- server/app.js
+++ server/app.js
@@ -12,6 +12,41 @@ app.get('/', (req,res) => {
 	res.send("connected to app")
 })
 
+app.get('/login', (req, res) => {
+	const username = "username";
+	const password  = "password";
+	let err = '';
+	User.find({username: username}, (issue, users)=>{
+		const user = users[0];
+		if(!(username && password)){
+			err = "Please fill in both fields";
+		}
+		else if(!user){
+			err = "Username does not exist";
+		}
+		if(err){
+			res.status(200).json({error: err});
+		}
+		//valid username
+		else{
+			hash({ password: password, salt: user.salt }, function (err, pass, salt, hash) {
+				if(err){
+					err = "Password and Username do not match";
+					res.status(200).json({error: err});
+				}
+        else if (hash === user.hash){
+					// req.session.user = user['_id']; add when we add session
+					res.json({'success':true});
+				}
+				else{
+					err = "Password and Username do not match";
+					res.status(200).json({error: err});
+				}
+      });
+		}
+	});
+});
+
 app.post('/create-account', (req, res) => {
 	const username = "username";
 	const password  = "password";
