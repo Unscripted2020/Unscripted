87482bff73a9b558fb8be0451e38366539fca82b
santiagorendon <sr4670@nyu.edu>
2020-06-08 22:44:38 -0400

    added helper functions

--- client/src/components/Browse.js
+++ client/src/components/Browse.js
@@ -16,56 +16,13 @@ import NavBar from './Navbar';
 import Footer from './Footer';
 //sign in modal
 import SignIn from './SignIn';
+//helper functions
+import { activateNavBar, initalize} from '../helperFunctions.js';
 
 class Browse extends React.Component{
   componentDidMount(){
-    let courses = [0, 1, 2];
-
-    $(document).ready(function(){
-        $("#loginButton").click(function(){
-            $("#loginModal").modal('show');
-        });
-        $("#loginClose").click(function(){
-            $("#loginModal").modal('hide');
-        });
-        $("#loginCancel").click(function(){
-            $("#loginModal").modal('hide');
-        });
-        $("#formButton").click(function(){
-            $("#formModal").modal('show');
-        });
-        $("#formClose").click(function(){
-            $("#formModal").modal('hide');
-        });
-        $("#formCancel").click(function(){
-            $("#formModal").modal('hide');
-        });
-        $("#course-1-close").click(function(){
-            $("#course-1").addClass('d-none');
-        });
-        $("#course-2-close").click(function(){
-            $("#course-2").addClass('d-none');
-        });
-        $("#course-3-close").click(function(){
-            $("#course-3").addClass('d-none');
-        });
-        $("#search-button").click(function(){
-
-        });
-        $("#mycarousel").carousel({interval: 2000});
-        $("#carouselButton").click(function(){
-            if ($("#carouselButton").children("span").hasClass('fa-pause')) {
-                $("#mycarousel").carousel('pause');
-                $("#carouselButton").children("span").removeClass('fa-pause');
-                $("#carouselButton").children("span").addClass('fa-play');
-            }
-            else {
-                $("#mycarousel").carousel('cycle');
-                $("#carouselButton").children("span").removeClass('fa-play');
-                $("#carouselButton").children("span").addClass('fa-pause');
-            }
-        });
-    });
+    activateNavBar(3);
+    initalize();
   }
   render(){
     return(
