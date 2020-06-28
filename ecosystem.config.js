b7bef9dc7c3fb36cc9cdea6af3acc5c383885a0d
AhmadAbroad22 <ahmadamali77@gmail.com>
2020-06-27 23:23:43 -0600

    inserted pm2 file for AWS deployment and added a few npm dependencies

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps : [{
+    name: 'UnscriptedBackEnd',
+    script: 'app.js',
+    autorestart: true,
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    },
+    watch: '.'
+  }, {
+    script: './service-worker/',
+    watch: ['./service-worker']
+  }],
+
+  deploy : {
+    production : {
+      key  : '~/.ssh/id_rsa.pub',
+      user : 'ec2-user',
+      host : 'ec2-18-217-46-116.us-east-2.compute.amazonaws.com',
+      ref  : 'origin/master',
+      repo : 'git@github.com:Unscripted2020/Unscripted.git',
+      path : '/home/ec2-user',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
